1dfe8eca9bc717c0020ccd23f643e634ddd94c17
voytenko.d <voytenko.d@ideasoft.io>
2019-09-06 16:08:18 +0300

    Setup app for Heroku

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = "Loading..";
     messageTwo.textContent = "";
 
-    fetch('http://localhost:4000/weather?address=' + location)
+    fetch('/weather?address=' + location)
     .then((response) => {
         response.json()
            .then((data) => {
