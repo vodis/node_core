22936119a6b3af30837e16195ca44b33395be4c6
voytenko.d <voytenko.d@ideasoft.io>
2019-09-06 16:24:45 +0300

    Add keys

---
+++ keys/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    geocodeToken: "pk.eyJ1Ijoidm9kaXMiLCJhIjoiY2p6cHVhaWo5MGpscDNtbzBremQyaGJnbyJ9.Ul42zhph2YIAob48rDywRA", 
+    forecastToken: "1bb448d32b4258cc62d6e1818fb89d83",
+}
